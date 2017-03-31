7a47e390bc97e5113e9c64c64b0b59c67d9064c8
Dan C Williams <dan.c.williams@gmail.com>
2017-03-31 03:44:07 +0000

    Inital commit...break for the night

--- linked_list/implementation.py
+++ linked_list/implementation.py
@@ -1,4 +1,5 @@
 from .interface import AbstractLinkedList
+from .node import Node
 
 
 class LinkedList(AbstractLinkedList):
@@ -7,7 +8,11 @@ class LinkedList(AbstractLinkedList):
     """
 
     def __init__(self, elements=None):
-        pass
+        self.start = None
+        self.end = None
+        if elements:
+            for elem in elements:
+                self.append(elem)        
 
     def __str__(self):
         pass
@@ -16,7 +21,19 @@ class LinkedList(AbstractLinkedList):
         pass
 
     def __iter__(self):
-        pass
+        self.current = self.start
+        return self
+        
+    def __next__(self):
+        if not self.current:
+            raise StopIteration()
+            
+        num = self.current
+        self.current += 1
+        
+        return num
+
+    __next__ = next
 
     def __getitem__(self, index):
         pass
@@ -28,13 +45,53 @@ class LinkedList(AbstractLinkedList):
         pass
 
     def __eq__(self, other):
-        pass
+        node_a = self.start
+        node_b = other.start
+
+        while True:
+            if not node_a and not node_b:
+                return True
+
+            if not bool(node_a) or not bool(node_b):
+                return False
+
+            if node_a.elem != node_b.elem:
+                return False
+
+            node_a = node_a.next
+            node_b = node_b.next    
 
     def append(self, elem):
-        pass
+        placeholder = Node(elem)
+        
+        if self.start is None:
+            self.start = placeholder
+            self.end = self.start
+        else:
+            self.end.next = placeholder
+            self.end = placeholder
+
 
     def count(self):
-        pass
+        counter = 0
+        
+        
+        
+        '''
+        for _ in self:
+            counter += 1
+        '''
+        return counter
 
     def pop(self, index=None):
         pass
+'''
+class Book(object):
+    self.value = 'x'
+pass
+
+
+placeholder = Node(value)
+self.end.next = placeholder
+self.end = placeholder
+'''
--- linked_list/node.py
+++ linked_list/node.py
@@ -4,13 +4,18 @@ class Node(object):
     """
 
     def __init__(self, elem, next=None):
-        pass
+        self.elem = elem
+        self.next = next
 
     def __str__(self):
-        pass
+        if self.next:
+            return 'Node({}) > Node({})'.format(self.elem, self.next.elem)
+        return r'Node({}) > /'.format(self.elem)
+
 
     def __eq__(self, other):
-        pass
-
+        return self.elem == other.elem
+        
+            
     def __repr__(self):
         pass
